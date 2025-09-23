7c416fe0e2b9d885ac945124d868d07381555f34
Paxton <paxtogrigoru@gmail.com>
2025-09-23 16:43:24 +0000

    tyth

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,9 @@
-print("Hello World!")
+array = [1, 2, 3, 4, 5]
+# list = ["i", '1', '{', 4, 'word']
+
+# for i in range(5):
+#     print(array[i], list[i], sep="")
+
+# print(array, "\n",  list, sep='')
+
+# print(array1)
