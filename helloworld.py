0e9163dffa43a5703a59c748c74bc7d61648fdc4
Paxton <paxtogrigoru@gmail.com>
2025-09-25 17:00:25 +0000

    yessss

--- helloworld.py
+++ helloworld.py
@@ -3,5 +3,4 @@ from array import array
 list1 = ['i', '1', '{', 4, 'word']
 arr1 = array('h', [-1, 2, 3, 4, 5])
 
-for i in range(5):
-    print(arr1[i], list1[i], sep="")
+print(arr1.tolist(), "\n", list1)
