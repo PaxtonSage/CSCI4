f9ed6a1fe0cf61db0320905ed4a65f82b171fb67
Paxton <paxtogrigoru@gmail.com>
2025-10-28 21:24:31 -0700

    comment???

--- recaman.py
+++ recaman.py
@@ -3,7 +3,7 @@ import os                                                        #imports the os
 def recaman(num):                                                #defines the function "recaman" with one argument called "num"
     list = []                                                    #creates a list with no members
     for n in range(0, num):                                      #begins a for loop with the iterator "n" from the range of 0 to "num"
-        if n == 0:                                               #checks if n is equal to 0, and if so...
+        if n == 0: #inefficient, but simpler imo                                              #checks if n is equal to 0, and if so...
             list.append(0)                                          #appends 0 to the end of list
         elif list[n-1] - n > 0 and not (list[n-1] - n in list):  #checks if the previous if statement didn't evaluate and if THAT is true
             list.append(list[n-1] - n)                              #appends (list[n-1] - n) to the end of list
