bfb31a3cb68b8def2d4d6cb0e340c74656701979
Paxton <paxtogrigoru@gmail.com>
2025-11-25 18:41:26 +0000

    euclid

---
+++ euclidean.py
@@ -0,0 +1,10 @@
+point1 = [0, 0] #define first point [x, y]
+point2 = [-3, -4] #define second point [x, y]
+
+def distance(p1, p2): #define function 'distance' which takes in two points, p1 and p2
+    dx = p1[0] - p2[0] #set the variable 'dx' equal to the horizontal distance between p1 and p2
+    dy = p1[1] - p2[1] #set the variable 'dy' equal to the vertical distance between the points
+    dist = pow((dx*dx) + (dy*dy), .5) #set the variable 'dist' equal to the distance between the two points using the pythagorean theorum with the horizontal and vertical distances being the inputs 
+    return dist #returns the distance between the points
+
+print(distance(point1, point2)) #prints out the distance between point1 and point2 using the distance function defined above
